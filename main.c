4730249f5e81133bfd4ccf1faae58a979a977045
raelaraujo0 <131907370+raelaraujo0@users.noreply.github.com>
2023-09-02 09:15:00 -0300

    Add files via upload
    remoção dos acentos pois estavam bugando ao rodar no CMD

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main();
 void sobre(void);
 void cadastrar_usuario(void);
 void deletar_usuario(void);
-void atualizar_usario(void);
+void atualizar_usuario(void);
 void ler_usuario(void);
 // funcoes
 
@@ -14,7 +14,7 @@ void ler_usuario(void);
 void sobre(void)
 {
     system("clear || cls");
-    pritnf("\n");
+    printf("\n");
     printf(" ==================================================================\n");
     printf(" ==================================================================\n");
     printf(" ===                                                            ===\n");
@@ -33,140 +33,240 @@ void sobre(void)
 
 void cadastrar_usuario(void)
 {
-    system("clear || cls");
-    printf("\n");
-    printf(" ==================================================================\n");
-    printf(" ==================================================================\n");
-    printf(" ===                                                            ===\n");
-    printf(" ===                 == CADASTRAR USUARIO ==                    ===\n");
-    prinft(" ===                                                            ===\n");
-    printf(" ===                     Nome completo:                         ===\n");
-    printf(" ===                     Id: (só numeros)                       ===\n");
-    printf(" ===               Data de Nascimento: (dd/mm/aaaa)             ===\n");
-    printf(" ===                   Telefone: (só numeros)                   ===\n");
-    printf(" ===                                                            ===\n");
-    printf(" ==================================================================\n");
-    printf(" ==================================================================\n");
-    printf("\n");
-    getchar();
-}
+    int telefone;
+    int datansm;
+    int id;
+    char nome[75];
+    char respt;
 
-void deletar_usuario(void);
-{
-    system("clear || cls");
-    pritnf("\n");
-    printf(" ==================================================================\n");
-    printf(" ==================================================================\n");
-    printf(" ===                                                            ===\n");
-    printf(" ===                   == DELETAR USUARIO ==                    ===\n");
-    prinft(" ===                                                            ===\n");
-    printf(" ===                     Id: (só numeros)                       ===\n");
-    printf(" ===                                                            ===\n");
-    printf(" ==================================================================\n");
-    printf(" ==================================================================\n");
-    pritnf("\n");
-    getchar();
-}
-
-void atualizar_usuario(void);
-{
-    system("clear || cls");
-    pritnf("\n");
-    printf(" ==================================================================\n");
-    printf(" ==================================================================\n");
-    printf(" ===                                                            ===\n");
-    printf(" ===                   == ATUALIZAR USUARIO ==                  ===\n");
-    prinft(" ===                                                            ===\n");
-    printf(" ===                     Id: (só numeros)                       ===\n");
-    printf(" ===                                                            ===\n");
-    printf(" ==================================================================\n");
-    printf(" ==================================================================\n");
-    pritnf("\n");
-    getchar();
-}
-
-void ler_usuario();
-{
-    system("clear || cls");
-    pritnf("\n");
-    printf(" ==================================================================\n");
-    printf(" ==================================================================\n");
-    printf(" ===                                                            ===\n");
-    printf(" ===                     == LER USUARIO ==                      ===\n");
-    prinft(" ===                                                            ===\n");
-    printf(" ===                     Id: (só numeros)                       ===\n");
-    printf(" ===                                                            ===\n");
-    printf(" ==================================================================\n");
-    printf(" ==================================================================\n");
-    pritnf("\n");
-    getchar();
-}
-
-int main()
-{
-    int escolha;
-    char resp;
-
-    do
+    while (1)
     {
         system("clear || cls");
         printf("\n");
         printf(" ==================================================================\n");
         printf(" ==================================================================\n");
         printf(" ===                                                            ===\n");
-        printf(" ===                   == MENU PRINCIPAL ==                     ===\n");
+        printf(" ===                 == CADASTRAR USUARIO ==                    ===\n");
         printf(" ===                                                            ===\n");
-        printf(" ===                     1. CRIAR USUARIO                       ===\n");
-        printf(" ===                                                            ===\n");
-        printf(" ===                   2. DELETAR USUARIO                       ===\n");
-        printf(" ===                                                            ===\n");
-        printf(" ===                   3. ATUALIZAR USUARIO                     ===\n");
-        printf(" ===                                                            ===\n");
-        printf(" ===                      4. LER USUARIO                        ===\n");
-        printf(" ===                                                            ===\n");
-        printf(" ===                         0. SOBRE                           ===\n");
-        printf(" ===                                                            ===\n");
-        printf(" ===                  Escolha uma das opcoes                    ===\n");
-        scanf("%d", &escolha);
+        printf(" ===                     Nome completo:                         ===\n");
+        printf(" ===                    Id: (só numeros)                        ===\n");
+        printf(" ===             Data de Nascimento: (dd/mm/aaaa)               ===\n");
+        printf(" ===                Telefone: (só numeros)                      ===\n");
         printf(" ===                                                            ===\n");
         printf(" ==================================================================\n");
         printf(" ==================================================================\n");
         printf("\n");
 
-        switch (escolha)
+        printf("Nome Completo:");
+        scanf("%s", nome);
+
+        printf("Id:");
+        scanf("%d", &id);
+
+        printf("Telefone:");
+        scanf("%d", &telefone);
+
+        getchar();
+
+        printf("Seja bem-vindo %s\n", nome);
+
+        printf("Deseja adicionar outro usuario?(S/N)");
+        scanf(" %c", &respt);
+
+        switch (respt)
         {
-        case 1:
-            // chamada da tela e funcoes
+        case 'S':
+        case 's':
             cadastrar_usuario();
             break;
-        case 2:
-            // chamada da tela e funcoes
+        case 'N':
+        case 'n':
+            main();
+            break;
+        default:
+            printf("Funcao invalida");
+        }
+    }
+}
+
+void deletar_usuario(void)
+{
+    int id;
+    char respt;
+    while (1)
+    {
+        system("clear || cls");
+        printf("\n");
+        printf(" ==================================================================\n");
+        printf(" ==================================================================\n");
+        printf(" ===                                                            ===\n");
+        printf(" ===                   == DELETAR USUARIO ==                    ===\n");
+        printf(" ===                                                            ===\n");
+        printf(" ===                     Id: (só numeros)                       ===\n");
+        printf(" ===                                                            ===\n");
+        printf(" ==================================================================\n");
+        printf(" ==================================================================\n");
+        printf("\n");
+
+        printf("Id:");
+        scanf("%d", &id);
+        getchar();
+        printf("Deseja deletar outro usuario?(S/N)");
+        scanf(" %c", &respt);
+        switch (respt)
+        {
+        case 'S':
+        case 's':
             deletar_usuario();
             break;
-        case 3:
-            // chamada da tela e funcoes
-            atualizar_usario();
+        case 'N':
+        case 'n':
+            main();
             break;
-        case 4:
-            // chamada da tela e funcoes
+        default:
+            printf("Funcao invalida");
+        }
+    }
+}
+
+void atualizar_usuario(void)
+{
+    int id;
+    char respt;
+    while (1)
+    {
+        system("clear || cls");
+        printf("\n");
+        printf(" ==================================================================\n");
+        printf(" ==================================================================\n");
+        printf(" ===                                                            ===\n");
+        printf(" ===                   == ATUALIZAR USUARIO ==                  ===\n");
+        printf(" ===                                                            ===\n");
+        printf(" ===                     Id: (só numeros)                       ===\n");
+        printf(" ===                                                            ===\n");
+        printf(" ==================================================================\n");
+        printf(" ==================================================================\n");
+        printf("\n");
+
+        printf("Id:");
+        scanf("%d", &id);
+        getchar();
+
+        printf("Deseja ler outro usuario?(S/N)");
+        scanf(" %c", &respt);
+        switch (respt)
+        {
+        case 'S':
+        case 's':
+            atualizar_usuario();
+            break;
+        case 'N':
+        case 'n':
+            main();
+            break;
+        default:
+            printf("Funcao invalida");
+        }
+    }
+}
+
+void ler_usuario(void)
+{
+    int id;
+    char respt;
+
+    while (1)
+    {
+        system("clear || cls");
+        printf("\n");
+        printf(" ==================================================================\n");
+        printf(" ==================================================================\n");
+        printf(" ===                                                            ===\n");
+        printf(" ===                     == LER USUARIO ==                      ===\n");
+        printf(" ===                                                            ===\n");
+        printf(" ===                     Id: (só numeros)                       ===\n");
+        printf(" ===                                                            ===\n");
+        printf(" ==================================================================\n");
+        printf(" ==================================================================\n");
+        printf("\n");
+
+        printf("Id:");
+        scanf("%d", &id);
+        getchar();
+
+        printf("Deseja atualizar outro usuario?(S/N)");
+        scanf(" %c", &respt);
+        switch (respt)
+        {
+        case 'S':
+        case 's':
             ler_usuario();
             break;
-        case 0:
-            // chamada da tela e funcoes
-            sobre();
+        case 'N':
+        case 'n':
+            main();
             break;
-
         default:
-            printf("opcao invalida \n");
-            break;
+            printf("Funcao invalida");
         }
-
-        if (escolha != 0)
-            ;
-        printf("quer realizar outra operacao? (S/N) ");
-        scanf("%c", resp);
     }
+}
 
-    while (resp == "S" || resp == "s");
-    return 0;
+
+int main()
+{
+    int escolha;
+    char resp;
+
+    system("clear || cls");
+    printf("\n");
+    printf(" ==================================================================\n");
+    printf(" ==================================================================\n");
+    printf(" ===                                                            ===\n");
+    printf(" ===                   == MENU PRINCIPAL ==                     ===\n");
+    printf(" ===                                                            ===\n");
+    printf(" ===                     1. CRIAR USUARIO                       ===\n");
+    printf(" ===                                                            ===\n");
+    printf(" ===                    2. DELETAR USUARIO                      ===\n");
+    printf(" ===                                                            ===\n");
+    printf(" ===                   3. ATUALIZAR USUARIO                     ===\n");
+    printf(" ===                                                            ===\n");
+    printf(" ===                      4. LER USUARIO                        ===\n");
+    printf(" ===                                                            ===\n");
+    printf(" ===                         0. SOBRE                           ===\n");
+    printf(" ===                                                            ===\n");
+    printf(" ===                  Escolha uma das opcoes                    ===\n");
+    printf(" ===                                                            ===\n");
+    printf(" ==================================================================\n");
+    printf(" ==================================================================\n");
+    scanf("%d", &escolha);
+    printf("\n");
+
+    switch (escolha)
+    {
+    case 1:
+        // chamada da tela e funcoes
+        cadastrar_usuario();
+        break;
+    case 2:
+        // chamada da tela e funcoes
+        deletar_usuario();
+        break;
+    case 3:
+        // chamada da tela e funcoes
+        atualizar_usuario();
+        break;
+    case 4:
+        // chamada da tela e funcoes
+        ler_usuario();
+        break;
+    case 0:
+        // chamada da tela e funcoes
+        sobre();
+        break;
+
+    default:
+        printf("opcao invalida \n");
+        break;
+    }
 }
