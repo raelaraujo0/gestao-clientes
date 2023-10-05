138cf220f6aa3d94d7688e4221b022940cac8177
Rael Araujo <131907370+raelaraujo0@users.noreply.github.com>
2023-10-05 17:16:07 -0300

    Add files via upload

---
+++ telas.h
@@ -0,0 +1,17 @@
+#ifndef TELAS_H
+#define TELAS_H
+
+void TelaSobre();
+void TelaPrincipal();
+void Tela_CadUsu();
+void Tela_DelUsu();
+void Tela_AttUsu();
+void Tela_LerUsu();
+
+void Tela_MenuV();
+void Tela_RegVen();
+void Tela_DelVen();
+void Tela_AttVen();
+void Tela_LerVen();
+
+#endif
