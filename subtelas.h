1b412a127a7fcf3037337abc8f7f8359c4626db6
Rael Araujo <131907370+raelaraujo0@users.noreply.github.com>
2023-10-12 11:05:08 -0300

    Add files via upload

---
+++ subtelas.h
@@ -0,0 +1,21 @@
+#ifndef SUBTELAS_H
+#define SUBTELAS_H
+
+void SubTelaCadUsu();
+
+void SubTelaDelUsu();
+
+void SubTelaAttUsu();
+
+void SubTelaLerUsu();
+
+
+void SubTelaRegVen();
+
+void SubTelaDelVen();
+
+void SubTelaAttVen();
+
+void SubTelaLerVen();
+
+#endif
