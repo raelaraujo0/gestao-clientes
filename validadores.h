505a9cb4cf1579b742a16840ce6682e81a5b495b
Rael Araujo <131907370+raelaraujo0@users.noreply.github.com>
2023-10-18 08:10:54 -0300

    Add files via upload

---
+++ validadores.h
@@ -0,0 +1,16 @@
+#ifndef VALIDADORES_H
+#define VALIDADORES_H
+
+void validardata();
+
+void validarnome();
+
+void validarcategoria();
+
+void validarcpf();
+
+void validartelefone();
+
+void validaremail();
+
+#endif
